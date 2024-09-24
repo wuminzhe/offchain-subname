a11c2faaa88a3cb6ea9941eba1d4b38c15905ecc
Aki Wu <wuminzhe@gmail.com>
2024-09-24 15:41:46 +0800

    add react and rainbowkit

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,54 @@
 import { Controller } from "@hotwired/stimulus"
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+const YourApp = () => {
+  return <ConnectButton />;
+};
+
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: '873f70fa626990b1ee3c14d55130a573',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: false,
+});
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <YourApp />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log("hello_controller connected")
+    ReactDOM.render(<App />, this.element);
   }
 }
